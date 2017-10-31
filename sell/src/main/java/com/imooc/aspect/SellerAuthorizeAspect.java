a1c34b21d2d2eaaf2bee945418de4d8c3749a391
zhangrui <zhangrui1@wecash.net>
2017-10-31 11:59:48 +0800

    Initial commit

---
+++ sell/src/main/java/com/imooc/aspect/SellerAuthorizeAspect.java
@@ -0,0 +1,42 @@
+package com.imooc.aspect;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by 廖师兄
+ * 2017-07-30 17:31
+ */
+@Aspect
+@Component
+@Slf4j
+public class SellerAuthorizeAspect {
+
+//    @Autowired
+//    private StringRedisTemplate redisTemplate;
+//
+//    @Pointcut("execution(public * com.imooc.controller.Seller*.*(..))" +
+//    "&& !execution(public * com.imooc.controller.SellerUserController.*(..))")
+//    public void verify() {}
+//
+//    @Before("verify()")
+//    public void doVerify() {
+//        ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+//        HttpServletRequest request = attributes.getRequest();
+//
+//        //查询cookie
+//        Cookie cookie = CookieUtil.get(request, CookieConstant.TOKEN);
+//        if (cookie == null) {
+//            log.warn("【登录校验】Cookie中查不到token");
+//            throw new SellerAuthorizeException();
+//        }
+//
+//        //去redis里查询
+//        String tokenValue = redisTemplate.opsForValue().get(String.format(RedisConstant.TOKEN_PREFIX, cookie.getValue()));
+//        if (StringUtils.isEmpty(tokenValue)) {
+//            log.warn("【登录校验】Redis中查不到token");
+//            throw new SellerAuthorizeException();
+//        }
+//    }
+}
